b6524add9b51256323a779f67e836323af6cabd0
Mac Computer <maccomputer@Macs-Air.home>
2020-08-29 13:17:22 +0300

    add inverse

--- kuvankasittely/kuvafiltteri.py
+++ kuvankasittely/kuvafiltteri.py
@@ -4,45 +4,42 @@
     Created on Wed Aug 26 19:30:39 2020
     
     @author: maccomputer
+    based on
+    https://www.codementor.io/@isaib.cicourel/image-manipulation-in-python-du1089j1u
 """
     
 from PIL import Image
     
-# Open Image
+
 def open_image(path):
     newImage = Image.open(path)
     return newImage
-    
-# Save Image
+
 def save_image(image, path):
     image.save(path, 'png')
   
-# Create a new image with the given size
+
 def create_image(i, j):
     image = Image.new("RGB", (i, j), color="white" )
     return image
 
-# Get the pixel from the given image
 def get_pixel(image, i, j):
-    # Inside image bounds?
+    
     width, height = image.size
     if i > width or j > height:
         return None
 
-    # Get Pixel
+    
     pixel = image.getpixel((i, j))
     return pixel
 
-# Create a Grayscale version of the image
-def convert_grayscale(image):
-    # Get size
-    width, height = image.size
 
-    # Create new Image and a Pixel Map
+def convert_to_gray(image):
+    width, height = image.size
     new = create_image(width, height)
     pixels = new.load()
 
-    # Transform to grayscale
+    
     for i in range(width):
         for j in range(height):
             # Get Pixel
@@ -50,25 +47,43 @@ def convert_grayscale(image):
             red =  pixel[0]
             green = pixel[1]
             blue = pixel[2]
-            gray = (red+green+blue)/3
+            gray = int((red+green+blue)/3)
    
-            # Transform to grayscale
-            #gray = (red * 0.299) + (green * 0.587) + (blue * 0.114)
-
-            # Set Pixel in new image
-            pixels[i, j] = (int(gray), int(gray), int(gray))
+            pixels[i, j] = (gray, gray, gray)
 
     new.show()
     return new
 
-# Main
-if __name__ == "__main__":
-    # Load Image (JPEG/JPG needs libjpeg to load)
-    original = open_image('kuva_matti.png')
+def invert_colors(image):
+    width, height = image.size
+    new = create_image(width, height)
+    pixels = new.load()
 
-    # Example Pixel Color
+    
+    for i in range(width):
+        for j in range(height):
+            # Get Pixel
+            pixel = get_pixel(image, i, j)
+            red =  abs(255-pixel[0])
+            green = abs(255-pixel[1])
+            blue = abs(255-pixel[2])
+   
+            pixels[i, j] = (red, green, blue)
+
+    new.show()
+    return new
+
+def main():
+    original = open_image('mtb_ruunaa.jpeg')
+    
+       
     print('Color: ' + str(get_pixel(original, 0, 0)))
+    
+    
+    new_gray = convert_to_gray(original)
+    save_image(new_gray, 'mtb_ruunaa_harmaa.jpeg')
+   
+    new_inverted = invert_colors(original)
+    save_image(new_inverted, 'mtb_ruunaa_inverted.jpeg')
 
-    # Convert to Grayscale and save
-    new = convert_grayscale(original)
-    save_image(new, 'kuva_matti_harmaa.png')
+main()
