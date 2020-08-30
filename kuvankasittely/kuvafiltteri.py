a44c6af0083eddef99cef11615241a0c6c47e554
Mac Computer <maccomputer@Macs-Air.home>
2020-08-30 17:10:59 +0300

    add examples

--- kuvankasittely/kuvafiltteri.py
+++ kuvankasittely/kuvafiltteri.py
@@ -4,11 +4,16 @@
     Created on Wed Aug 26 19:30:39 2020
     
     @author: maccomputer
-    based on
+    Documentation of PILLOW: https://pillow.readthedocs.io/en/5.1.x/index.html
+    
+    tutorial: 
+    https://pillow.readthedocs.io/en/3.0.0/handbook/tutorial.html#geometrical-transforms
+    
+    examples:
     https://www.codementor.io/@isaib.cicourel/image-manipulation-in-python-du1089j1u
 """
     
-from PIL import Image
+from PIL import Image, ImageFilter
     
 
 def open_image(path):
@@ -51,7 +56,7 @@ def convert_to_gray(image):
    
             pixels[i, j] = (gray, gray, gray)
 
-    new.show()
+    #new.show()
     return new
 
 def invert_colors(image):
@@ -64,26 +69,76 @@ def invert_colors(image):
         for j in range(height):
             # Get Pixel
             pixel = get_pixel(image, i, j)
-            red =  abs(255-pixel[0])
-            green = abs(255-pixel[1])
-            blue = abs(255-pixel[2])
+            red =  255 - pixel[0]
+            green = 255 - pixel[1]
+            blue = 255 - pixel[2]
    
             pixels[i, j] = (red, green, blue)
 
-    new.show()
+    #new.show()
     return new
 
+
 def main():
-    original = open_image('mtb_ruunaa.jpeg')
+    original = open_image('bike.jpg')
     
        
     print('Color: ' + str(get_pixel(original, 0, 0)))
     
     
-    new_gray = convert_to_gray(original)
-    save_image(new_gray, 'mtb_ruunaa_harmaa.jpeg')
+    im1 = convert_to_gray(original)
+    im1.show()
+    #save_image(im1, 'imgage_gray.jpg')
    
-    new_inverted = invert_colors(original)
-    save_image(new_inverted, 'mtb_ruunaa_inverted.jpeg')
+    im2 = invert_colors(original)
+    im2.show()
+    #save_image(im2, 'image_inverted.jpg')
+    
+    '''
+    FILTERS: 
+        BLUR, 
+        CONTOUR
+        DETAIL
+        EDGE_ENHANCE
+        EDGE_ENHANCE_MORE
+        EMBOSS
+        FIND_EDGES
+        SHARPEN
+        SMOOTH
+        SMOOTH_MORE
+    '''
+    im3 = original.filter(ImageFilter.CONTOUR)
+    im3.show()
+    
+    im4 = original.filter(ImageFilter.FIND_EDGES)
+    im4.show()
+    
+    im5 = original.filter(ImageFilter.EMBOSS)
+    im5.show()
+    
+    im6 = original.filter(ImageFilter.EDGE_ENHANCE)
+    im6.show()
+    
+    '''
+    Transposing of image:
+    transpose(Image.FLIP_LEFT_RIGHT)
+    FLIP_TOP_BOTTOM    
+    FLIP_LEFT_RIGHT
+    ROTATE_90, ROTATE_180
+    
+    img.rotate(90)
+    img.resize((256, 256))
+    
+    
+    Crop:
+    borders = (200, 200, 800, 800)
+    crop = img.crop(borders)
+    '''
+    
+    im6 = original.transpose(Image.FLIP_LEFT_RIGHT)
+    im6.show()
+    
+    
+    
 
 main()
